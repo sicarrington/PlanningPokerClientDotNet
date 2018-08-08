dade639441318eb5b5242f598f4b99f4cd2835fc
Simon <si.carrington@outlook.com>
2018-08-08 14:33:51 +0100

    Upgrade coveralls tooling

--- coveralls.sh
+++ coveralls.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
-  dotnet tool install coveralls.net --version 1.0.0 --tools-path tools
+  dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
   ./tools/csmacnz.Coveralls --opencover -i ./PlanningPoker.Client/PlanningPoker.Client.Tests/coverage.opencover.xml --useRelativePaths
 
   # nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
