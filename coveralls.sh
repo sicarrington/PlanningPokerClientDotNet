3e62ed426df60444c1efa93412b2d935110b428b
Simon Carrington <simon.carrington@gmail.com>
2022-12-27 18:23:17 +0000

    disable

--- coveralls.sh
+++ coveralls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -n "$COVERALLS_REPO_TOKEN" ]
-then
-  dotnet tool install coveralls.net --version 2.0.0 --tool-path tools
-  ./tools/csmacnz.Coveralls --opencover -i ./PlanningPoker.Client/PlanningPoker.Client.Tests/coverage.opencover.xml --useRelativePaths
-fi
+# if [ -n "$COVERALLS_REPO_TOKEN" ]
+# then
+#   dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
+#   ./tools/csmacnz.Coveralls --opencover -i ./PlanningPoker.Client/PlanningPoker.Client.Tests/coverage.opencover.xml --useRelativePaths
+# fi
