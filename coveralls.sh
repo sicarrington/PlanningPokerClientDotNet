0919da4c00b21723d9fd15a21310cf5d3e8d7ffc
Simon Carrington <simon.carrington@gmail.com>
2022-12-27 18:21:35 +0000

    update tool

--- coveralls.sh
+++ coveralls.sh
@@ -2,6 +2,6 @@
 
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
-  dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
+  dotnet tool install coveralls.net --version 2.0.0 --tool-path tools
   ./tools/csmacnz.Coveralls --opencover -i ./PlanningPoker.Client/PlanningPoker.Client.Tests/coverage.opencover.xml --useRelativePaths
 fi
